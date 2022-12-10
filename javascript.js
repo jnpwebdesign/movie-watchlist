bf5394e3c2fac21cd984f0ff4a0ff1ffbd7ef68a
Julie Napear <72626295+jnpwebdesign@users.noreply.github.com>
2022-12-10 11:41:33 -0500

    12/10/22 11:41 search working

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ const form = document.getElementById("search-movies-form");
 const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
 const searchResultsContainer = document.querySelector("main");
-let detailedMovieArray = [];
+let watchListArray = [];
 
 //search button searches API for the input from user, returns array of objects with minimal properties for each movie
 form.addEventListener("submit", function(e) {
@@ -18,12 +18,14 @@ form.addEventListener("submit", function(e) {
 });
 
 //takes the array from Event Listener above and loops through to fetch additional information about each movie. 
-function getDetailedMovieData(movieArray) {
+async function getDetailedMovieData(movieArray) {
+    let data = "";
 
-    for (let movie of movieArray) {
-        fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=c92898b8`)
-            .then(res => res.json())
-            .then (data => {
+    for (let i = 0; i < movieArray.length; i++) {
+        const res = await fetch(`http://www.omdbapi.com/?i=${movieArray[i].imdbID}&apikey=c92898b8`)
+        data = await res.json()
+        console.log(data);
+        renderHTML(data);
                 searchResultsContainer.innerHTML += `
                 <div class="movie-container" id="movie-container">
                     <img src="${data.Poster}" class="movie-poster" height="147px" width="99px">
@@ -34,21 +36,56 @@ function getDetailedMovieData(movieArray) {
                         <div class="movie-info-container">
                             <p class="run-time"><span class="number-of-minutes">${data.Runtime}</span></p>
                             <p class="genres">${data.Genre}</p>
-                            <button class="add-to-watchlist-btn"><img src="images/plus-button.png" class="plus-icon">Watchlist</button>
+                            <button class="add-to-watchlist-btn" id="add-to-watchlist-btn" data-add="${movieArray[i].imdbID}"><img src="images/plus-button.png" class="plus-icon" data-add${i}="${movieArray[i].imdbID}">Watchlist</button>
                         </div>
                         <p class="movie-synopsis">${data.Plot}</p>
                     </div>
                 </div>
                 `
-            })
-            
-    };
+    };    
 
-    
+    document.addEventListener("click", function(e) {
+        
+        if(e.target.dataset.add0) {
+            console.log("click");
+            watchListArray.push(movieArray[0].imdbID)
+            console.log(watchListArray);
+        } else if (e.target.dataset.add1) {
+            console.log("click");
+            watchListArray.push(movieArray[1].imdbID)
+            console.log(watchListArray);
+        } else if (e.target.dataset.add2) {
+            console.log("click");
+            watchListArray.push(movieArray[2].imdbID)
+            console.log(watchListArray);
+        } else if (e.target.dataset.add3) {
+            console.log("click");
+            watchListArray.push(movieArray[3].imdbID)
+            console.log(watchListArray);
+        } else if (e.target.dataset.add4) {
+            console.log("click");
+            watchListArray.push(movieArray[4].imdbID)
+            console.log(watchListArray);
+        } else if (e.target.dataset.add5) {
+            console.log("click");
+            watchListArray.push(movieArray[5].imdbID)
+            console.log(watchListArray);
+        } else if (e.target.dataset.add6) {
+            console.log("click");
+            watchListArray.push(movieArray[6].imdbID)
+            console.log(watchListArray);   
+        }else if (e.target.dataset.add7) {
+            console.log("click");
+            watchListArray.push(movieArray[7].imdbID)
+            console.log(watchListArray);
+        } else if (e.target.dataset.add8) {
+            console.log("click");
+            watchListArray.push(movieArray[8].imdbID)
+            console.log(watchListArray);
+        } else if (e.target.dataset.add9) {
+            console.log("click");
+            watchListArray.push(movieArray[9].imdbID)
+            console.log(watchListArray);
+        }
+    });
 }
-
-
-function renderHTML(detailedMovieArray) {
-    console.log("detailed movie arr")};
-
-
