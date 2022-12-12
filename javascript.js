555f32d4451d421630d545325b96eac860ccb967
Julie Napear <72626295+jnpwebdesign@users.noreply.github.com>
2022-12-12 09:58:34 -0500

    12/12/22 09:58 draft

--- javascript.js
+++ javascript.js
@@ -5,6 +5,8 @@ const searchInput = document.getElementById("search-input");
 const searchBtn = document.getElementById("search-btn");
 const searchResultsContainer = document.querySelector("main");
 let watchListArray = [];
+let watchListArrayDetails = [];
+
 
 //search button searches API for the input from user, returns array of objects with minimal properties for each movie
 form.addEventListener("submit", function(e) {
@@ -29,10 +31,11 @@ async function getDetailedMovieData(movieArray) {
     renderHTML(detailedMovieArray); 
 }
 
+//prints detailed movie information about each movie
 function renderHTML(detailedMovieArray) {
     for (let i = 0; i < detailedMovieArray.length; i++) {
         searchResultsContainer.innerHTML += `
-            <div class="movie-container" id="movie-container">
+            <div class="movie-container" id="${detailedMovieArray[i].Title}-container">
                 <img src="${detailedMovieArray[i].Poster}" class="movie-poster">
                 <div class="movie-details-container">
                     <div class="movie-title-container">
@@ -41,163 +44,68 @@ function renderHTML(detailedMovieArray) {
                     <div class="movie-info-container">
                         <p class="run-time"><span class="number-of-minutes">${detailedMovieArray[i].Runtime}</span></p>
                         <p class="genres">${detailedMovieArray[i].Genre}</p>
-                        <button class="add-to-watchlist-btn" data-add${i}="${detailedMovieArray[i].imdbID}" id="add-to-watchlist-btn${i}"><img src="images/plus-button.png" class="plus-icon" id="watchlist-btn-image${i}" data-add${i}="${detailedMovieArray[i].imdbID}">Watchlist</button>
+                        <button class="add-to-watchlist-btn" data-imdb="${detailedMovieArray[i].imdbID}" id="add-to-watchlist-btn-${detailedMovieArray[i].imdbID}"><img src="images/plus-button.png" class="plus-icon" id="watchlist-btn-image-${detailedMovieArray[i].imdbID}" data-imdb="${detailedMovieArray[i].imdbID}">Watchlist</button>
                     </div>
                     <p class="movie-synopsis">${detailedMovieArray[i].Plot}</p>
                 </div>
             </div>
         `
     }
-    addToWatchlist(detailedMovieArray);
+    chooseWatchlistMovies();
 }
 
-function addToWatchlist(detailedMovieArray) {
-    document.addEventListener("click", function(e) {
-            
-        if(e.target.dataset.add0) {
-            if  (document.getElementById("watchlist-btn-image0").disabled) {
-                watchListArray.splice(watchListArray.indexOf(detailedMovieArray[0].imdbID, 1));
-                document.getElementById("watchlist-btn-image0").disabled = false;
-                document.getElementById("watchlist-btn-image0").classList.toggle("grayed-out");
-                document.getElementById("add-to-watchlist-btn0").classList.toggle("grayed-out");
+
+//adds/deletes chosen movies to watchlist
+function chooseWatchlistMovies() {
+
+    searchResultsContainer.addEventListener("click", function(e) { 
+
+        if (e.target.dataset.imdb) {
+             if(watchListArray.indexOf(e.target.dataset.imdb) === -1) {
+                watchListArray.push(e.target.dataset.imdb);
+                document.getElementById(`watchlist-btn-image-${e.target.dataset.imdb}`).disabled;
+                document.getElementById(`add-to-watchlist-btn-${e.target.dataset.imdb}`).disabled;
+                document.getElementById(`add-to-watchlist-btn-${e.target.dataset.imdb}`).classList.toggle("grayed-out");
+                document.getElementById(`watchlist-btn-image-${e.target.dataset.imdb}`).classList.toggle("grayed-out");
                 console.log(watchListArray);
-            } else {
-                watchListArray.push(detailedMovieArray[0].imdbID);
-                document.getElementById("watchlist-btn-image0").classList.toggle("grayed-out");
-                document.getElementById("watchlist-btn-image0").disabled = true;
-                document.getElementById("add-to-watchlist-btn0").classList.toggle("grayed-out");
+             } else  if (watchListArray.indexOf(e.target.dataset.imdb) >= 0 ) {
+                document.getElementById(`add-to-watchlist-btn-${e.target.dataset.imdb}`).classList.toggle("grayed-out");
+                document.getElementById(`watchlist-btn-image-${e.target.dataset.imdb}`).classList.toggle("grayed-out");
+                document.getElementById(`watchlist-btn-image-${e.target.dataset.imdb}`).disabled === false;
+                document.getElementById(`add-to-watchlist-btn-${e.target.dataset.imdb}`).disabled === false;
+                watchListArray.splice(watchListArray.indexOf(e.target.dataset.imdb), 1);
                 console.log(watchListArray);
-            }
-        } else if (e.target.dataset.add1) {
-            if  (document.getElementById("watchlist-btn-image1").disabled) {
-                watchListArray.splice(watchListArray.indexOf(detailedMovieArray[1].imdbID, 1));
-                document.getElementById("watchlist-btn-image1").disabled = false;
-                document.getElementById("watchlist-btn-image1").classList.toggle("grayed-out");
-                document.getElementById("add-to-watchlist-btn1").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            } else {
-                watchListArray.push(detailedMovieArray[1].imdbID);
-                document.getElementById("watchlist-btn-image1").classList.toggle("grayed-out");
-                document.getElementById("watchlist-btn-image1").disabled = true;
-                document.getElementById("add-to-watchlist-btn1").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            }    
-        } else if (e.target.dataset.add2) {
-            if  (document.getElementById("watchlist-btn-image2").disabled) {
-                watchListArray.splice(watchListArray.indexOf(detailedMovieArray[2].imdbID, 1));
-                document.getElementById("watchlist-btn-image2").disabled = false;
-                document.getElementById("watchlist-btn-image2").classList.toggle("grayed-out");
-                document.getElementById("add-to-watchlist-btn2").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            } else {
-                watchListArray.push(detailedMovieArray[2].imdbID);
-                document.getElementById("watchlist-btn-image2").classList.toggle("grayed-out");
-                document.getElementById("watchlist-btn-image2").disabled = true;
-                document.getElementById("add-to-watchlist-btn2").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            }       
-        } else if (e.target.dataset.add3) {
-            if  (document.getElementById("watchlist-btn-image3").disabled) {
-                watchListArray.splice(watchListArray.indexOf(detailedMovieArray[3].imdbID, 1));
-                document.getElementById("watchlist-btn-image3").disabled = false;
-                document.getElementById("watchlist-btn-image3").classList.toggle("grayed-out");
-                document.getElementById("add-to-watchlist-btn3").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            } else {
-                watchListArray.push(detailedMovieArray[3].imdbID);
-                document.getElementById("watchlist-btn-image3").classList.toggle("grayed-out");
-                document.getElementById("watchlist-btn-image3").disabled = true;
-                document.getElementById("add-to-watchlist-btn3").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            }       
-        } else if (e.target.dataset.add4) {
-            if  (document.getElementById("watchlist-btn-image4").disabled) {
-                watchListArray.splice(watchListArray.indexOf(detailedMovieArray[4].imdbID, 1));
-                document.getElementById("watchlist-btn-image4").disabled = false;
-                document.getElementById("watchlist-btn-image4").classList.toggle("grayed-out");
-                document.getElementById("add-to-watchlist-btn4").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            } else {
-                watchListArray.push(detailedMovieArray[4].imdbID);
-                document.getElementById("watchlist-btn-image4").classList.toggle("grayed-out");
-                document.getElementById("watchlist-btn-image4").disabled = true;
-                document.getElementById("add-to-watchlist-btn4").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            }       
-        } else if (e.target.dataset.add5) {
-            if  (document.getElementById("watchlist-btn-image5").disabled) {
-                watchListArray.splice(watchListArray.indexOf(detailedMovieArray[5].imdbID, 1));
-                document.getElementById("watchlist-btn-image5").disabled = false;
-                document.getElementById("watchlist-btn-image5").classList.toggle("grayed-out");
-                document.getElementById("add-to-watchlist-btn5").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            } else {
-                watchListArray.push(detailedMovieArray[5].imdbID);
-                document.getElementById("watchlist-btn-image5").classList.toggle("grayed-out");
-                document.getElementById("watchlist-btn-image5").disabled = true;
-                document.getElementById("add-to-watchlist-btn5").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            }       
-        } else if (e.target.dataset.add6) {
-            if  (document.getElementById("watchlist-btn-image6").disabled) {
-                watchListArray.splice(watchListArray.indexOf(detailedMovieArray[6].imdbID, 1));
-                document.getElementById("watchlist-btn-image6").disabled = false;
-                document.getElementById("watchlist-btn-image6").classList.toggle("grayed-out");
-                document.getElementById("add-to-watchlist-btn6").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            } else {
-                watchListArray.push(detailedMovieArray[6].imdbID);
-                document.getElementById("watchlist-btn-image6").classList.toggle("grayed-out");
-                document.getElementById("watchlist-btn-image6").disabled = true;
-                document.getElementById("add-to-watchlist-btn6").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            }       
-        } else if (e.target.dataset.add7) {
-            if  (document.getElementById("watchlist-btn-image7").disabled) {
-                watchListArray.splice(watchListArray.indexOf(detailedMovieArray[7].imdbID, 1));
-                document.getElementById("watchlist-btn-image7").disabled = false;
-                document.getElementById("watchlist-btn-image7").classList.toggle("grayed-out");
-                document.getElementById("add-to-watchlist-btn7").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            } else {
-                watchListArray.push(detailedMovieArray[7].imdbID);
-                document.getElementById("watchlist-btn-image7").classList.toggle("grayed-out");
-                document.getElementById("watchlist-btn-image7").disabled = true;
-                document.getElementById("add-to-watchlist-btn7").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            }       
-        } else if (e.target.dataset.add8) {
-            if  (document.getElementById("watchlist-btn-image8").disabled) {
-                watchListArray.splice(watchListArray.indexOf(detailedMovieArray[8].imdbID, 1));
-                document.getElementById("watchlist-btn-image8").disabled = false;
-                document.getElementById("watchlist-btn-image8").classList.toggle("grayed-out");
-                document.getElementById("add-to-watchlist-btn8").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            } else {
-                watchListArray.push(detailedMovieArray[8].imdbID);
-                document.getElementById("watchlist-btn-image8").classList.toggle("grayed-out");
-                document.getElementById("watchlist-btn-image8").disabled = true;
-                document.getElementById("add-to-watchlist-btn8").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            }       
-        } else if (e.target.dataset.add9) {
-            if  (document.getElementById("watchlist-btn-image9").disabled) {
-                watchListArray.splice(watchListArray.indexOf(detailedMovieArray[9].imdbID, 1));
-                document.getElementById("watchlist-btn-image9").disabled = false;
-                document.getElementById("watchlist-btn-image9").classList.toggle("grayed-out");
-                document.getElementById("add-to-watchlist-btn9").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            } else {
-                watchListArray.push(detailedMovieArray[9].imdbID);
-                document.getElementById("watchlist-btn-image9").classList.toggle("grayed-out");
-                document.getElementById("watchlist-btn-image9").disabled = true;
-                document.getElementById("add-to-watchlist-btn9").classList.toggle("grayed-out");
-                console.log(watchListArray);
-            }     
+             }
+    
         }
     });
 }
 
-function viewWatchlist() {
+
+
+
+
+
+async function getWatchList(watchListArray) {
+    if (watchListArray.length === 0){
+        searchResultsContainer.innerHTML = `<p>No watchlist</p>`
+        console.log("click");
+    }
+    console.log("click");
     console.log(watchListArray);
+    let data = "";
+    searchResultsContainer.innerHTML = "";
+    let detailedMovieArray = [];
+    for (let i = 0; i < watchListArray.length; i++) {
+        const res = await fetch(`http://www.omdbapi.com/?i=${watchListArray[i]}&apikey=c92898b8`);
+        data = await res.json();
+        watchListArrayDetails.push(data);
+    }   
+    renderHTML(watchListArrayDetails); 
 }
+
+document.getElementById("view-my-watchlist-btn").addEventListener("click", function(watchListArray){
+    console.log(watchListArray);
+}); 
+
+
