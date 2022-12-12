d239d8627360e1024a722ed2bcea618ed6f6926e
Julie Napear <72626295+jnpwebdesign@users.noreply.github.com>
2022-12-12 15:35:15 -0500

    12/12/22 15:34 draft with watchListArray

--- javascript.js
+++ javascript.js
@@ -55,7 +55,7 @@ function renderHTML(detailedMovieArray) {
 }
 
 
-//adds/deletes chosen movies to watchlist
+//adds/deletes chosen movies to watchlist and local storage
 function chooseWatchlistMovies(detailedMovieArray) {
 
     searchResultsContainer.addEventListener("click", function(e) { 
@@ -86,53 +86,60 @@ function chooseWatchlistMovies(detailedMovieArray) {
                 }  
                 console.log(watchListArray);
             }
-    
         }
     });
+    return watchListArray;
 }
 
-
-
-
-
-
-async function getWatchList(watchListArray) {
-    if (watchListArray.length === 0){
-        searchResultsContainer.innerHTML = `<p>No watchlist</p>`
-        console.log("click");
-    }
-    console.log("click");
-    console.log(watchListArray);
-    let data = "";
-    searchResultsContainer.innerHTML = "";
-    let detailedMovieArray = [];
-    for (let i = 0; i < watchListArray.length; i++) {
-        const res = await fetch(`http://www.omdbapi.com/?i=${watchListArray[i]}&apikey=c92898b8`);
-        data = await res.json();
-        watchListArrayDetails.push(data);
-    }   
-    renderHTML(watchListArrayDetails); 
+//display empty watchList message
+const displayEmptyWatchListMessage = () => {
+        console.log("empty")
+            searchResultsContainer.innerHTML = `
+                <p class="no-watchlist">No watchlist yet. 
+                    <br> 
+                    <br> Use the box above to search for movies, then click the <img src="images/plus-button.png" class="plus-icon-watchlist"> Watchlist button to add them to your watchlist.
+                </p>
+            `
 }
+    
+
 
 document.getElementById("view-my-watchlist-btn").addEventListener("click", function(e){
-    if (watchListArray.length === 0){
-        searchResultsContainer.innerHTML = `
-            <p class="no-watchlist">No watchlist yet. 
-                <br> 
-                <br> Use the box above to search for movies, then click the <img src="images/plus-button.png" class="plus-icon-watchlist"> Watchlist button to add them to your watchlist.
-            </p>
-        `
+    if (localStorage.length === 0){
+        displayEmptyWatchListMessage();
     } else {
+        let watchListKeysArray = Object.keys(localStorage);
+        let watchListObjectArray = watchListKeysArray.map(function(key) {
+        return JSON.parse(localStorage.getItem(key));
+        })    
+        console.log(watchListObjectArray);
         searchResultsContainer.innerHTML = "";
-        for (let i = 0; i < watchListArray.length; i++) {
-            fetch(`http://www.omdbapi.com/?i=${watchListArray[i].imdbID}&apikey=c92898b8`)
-                .then (res => res.json())
-                .then(data => {
-                    console.log(data);
-                });
-        }   
+        renderWatchListHTML(watchListObjectArray);
     }
 
-}); 
+})
 
 
+function renderWatchListHTML(detailedMovieArray) {
+    for (let i = 0; i < detailedMovieArray.length; i++) {
+        console.log(detailedMovieArray);
+        searchResultsContainer.innerHTML += `
+            <div class="movie-container" id="${detailedMovieArray[i].Title}-container">
+                <img src="${detailedMovieArray[i].Poster}" class="movie-poster">
+                <div class="movie-details-container">
+                    <div class="movie-title-container">
+                        <h2 class="movie-title">${detailedMovieArray[i].Title}<img src="images/imdb-star.png" class="star-icon"><span class="imdb-rating">${detailedMovieArray[i].Ratings[0].Value}</span></h2>
+                    </div>
+                    <div class="movie-info-container">
+                        <p class="run-time"><span class="number-of-minutes">${detailedMovieArray[i].Runtime}</span></p>
+                        <p class="genres">${detailedMovieArray[i].Genre}</p>
+                        <button class="add-to-watchlist-btn" data-imdb="${detailedMovieArray[i].imdbID}" id="add-to-watchlist-btn-${detailedMovieArray[i].imdbID}"><img src="images/plus-button.png" class="plus-icon" id="watchlist-btn-image-${detailedMovieArray[i].imdbID}" data-imdb="${detailedMovieArray[i].imdbID}">Watchlist</button>
+                    </div>
+                    <p class="movie-synopsis">${detailedMovieArray[i].Plot}</p>
+                </div>
+            </div>
+        `
+    }
+ //   chooseWatchlistMovies(detailedMovieArray);
+}
+ 
